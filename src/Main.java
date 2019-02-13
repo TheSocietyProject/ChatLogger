b254bc5ef95e811103f0abe961c5974d4eaee67c
Society Project <46810642+TheSocietyProject@users.noreply.github.com>
2019-02-13 16:51:28 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@
 
 import com.sasha.eventsys.SimpleEventHandler;
 import com.sasha.eventsys.SimpleListener;
-import com.sasha.reminecraft.ReMinecraft;
+import com.sasha.reminecraft.Configuration;
 import com.sasha.reminecraft.api.RePlugin;
 import com.sasha.reminecraft.api.event.ChatReceivedEvent;
 import com.sasha.reminecraft.client.ReClient;
 import com.sasha.reminecraft.logging.ILogger;
 import com.sasha.reminecraft.logging.LoggerBuilder;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
@@ -23,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Main extends RePlugin implements SimpleListener {
 
+
+    private Config CFG = new Config();
 
     private ArrayList<String> data = new ArrayList<>();
 
@@ -70,11 +69,95 @@ public class Main extends RePlugin implements SimpleListener {
         data += ": ";
         data += msg;
 
+        if(CFG.var_whatToDoWithSpam != 0)
+            data = prepareIfSpam(data, CFG.var_whatToDoWithSpam);
+
+        // if it is spam and should be left out its null so return if null
+        if(data == null)
+            return;
+
 
         this.data.add(data);
 
     }
 
+    private String prepareIfSpam(String rV, int whatToDoWithSpam) {
+        int spam = isSpam(rV);
+
+        if(spam < 0) // only values < 0 r no spam
+            return rV;
+
+        if(whatToDoWithSpam == 1)
+            return null;
+
+        if(whatToDoWithSpam == 2) // P for pointer so u know what orig msg it was
+            return "P(" + spam + "): " + rV.split(": ")[0];
+
+        return rV;
+    }
+
+    /**
+     *
+     * @param rV
+     * @return where the repeated msg is found
+     */
+    private int isSpam(String rV) {
+        try {
+            FileReader fr = new FileReader(getFilepath());
+            BufferedReader br = new BufferedReader(fr);
+
+            int length = getLength(fr);
+            int index = CFG.var_howOldIsSpam;
+
+            int toSkip = length - index;
+            if(toSkip < 0){
+                toSkip = 0;
+            }
+            br.skip(toSkip);
+
+            while(index < length) {
+                if(rV.equals(br.readLine().split(": ")[1])){
+
+                    return index;
+                }
+
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int getLength(FileReader input){
+        int rV = 0;
+        try {
+            LineNumberReader count = new LineNumberReader(input);
+            while (count.skip(Long.MAX_VALUE) > 0) {
+                // Loop just in case the file is > Long.MAX_VALUE or skip() decides to not read the entire file
+            }
+
+            rV = count.getLineNumber() + 1;                                    // +1 because line index starts at 0
+        } catch(Exception e){}
+        return rV;
+    }
+
+    public String getFilepath(){
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+
+        String filename = s + "/data/ChatLog" + LocalDateTime.now().getYear() + "-" + LocalDateTime.now().getMonthValue() + "-" + LocalDateTime.now().getDayOfMonth() + ".txt";
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            new File(s + "\\data").mkdir();//.createNewFile();
+        }
+
+        return filename;
+    }
 
 
     /**
@@ -83,19 +166,9 @@ public class Main extends RePlugin implements SimpleListener {
      */
     private void writeToFile() {
 
-
-        Path currentRelativePath = Paths.get("");
-        String s = currentRelativePath.toAbsolutePath().toString();
-
-        String filename = s + "/data/ChatLog" + LocalDateTime.now().getYear() + "-" + LocalDateTime.now().getMonthValue() + "-" + LocalDateTime.now().getDayOfMonth() + ".txt";
-        logger.log("[ChatLogger]: flushing data: " + data.size() + " in " + filename);
-        try {
-
-            File f = new File(filename);
-            if(!f.exists()){
-                new File(s + "\\data").mkdir();//.createNewFile();
-            }
-            FileWriter fw = new FileWriter(filename, true);
+        try{
+            logger.log("[ChatLogger]: flushing data: " + data.size());
+            FileWriter fw = new FileWriter(getFilepath(), true);
             BufferedWriter bw = new BufferedWriter(fw);
             for (String line : data) {
                 bw.newLine();
@@ -113,18 +186,28 @@ public class Main extends RePlugin implements SimpleListener {
         }
     }
 
-
-    private void testTabFile(){
-
-
-
-
-
-    }
-
     @Override
     public void registerConfig() {
-
+        this.getReMinecraft().configurations.add(CFG);
     }
+}
+
+
+class Config extends Configuration {
+    @ConfigSetting
+    public int var_whatToDoWithSpam; // 0 does nothing, 1 removes it completely, 2 points to the msg that is repeated
+
+    @ConfigSetting
+    public int var_howOldIsSpam; // how long to go back to compare whether msg is repeated
+
+    public Config() {
+        super("ReMinecraft");
+
+        this.var_whatToDoWithSpam = 2;
+        this.var_howOldIsSpam = Integer.MAX_VALUE;
+    }
+
+
+
 }
 
